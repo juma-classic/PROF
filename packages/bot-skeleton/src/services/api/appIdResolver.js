514b78381a24e7afa3f6b982f2fa9769736809f9
juma-classic <138252099+juma-classic@users.noreply.github.com>
2026-03-12 22:03:49 +0300

    Initial commit

---
+++ packages/bot-skeleton/src/services/api/appIdResolver.js
@@ -0,0 +1,9 @@
+const AppIds = Object.freeze({
+    'deriv.app': '129365', // TODO: [app-link-refactor] - Remove backwards compatibility for `deriv.app`
+    'staging.deriv.app': '129365',
+    'app.deriv.com': '129365',
+    'staging-app.deriv.com': '129365',
+    'goon-puce.vercel.app': '129365',
+});
+
+export default AppIds;
